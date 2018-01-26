9cdd8b8ca3235fde3734fe89823a9c08bb0aea13
lzh <1216083447@qq.com>
2018-01-26 15:01:12 +0800

    将ganzhiwuxin.py中的模块合并到__init__.py命名空间

---
+++ ganzhiwuxin/__init__.py
@@ -0,0 +1,1 @@
+from .ganzhiwuxin import *
--- setup.py
+++ setup.py
@@ -3,10 +3,13 @@ setup(
     name = "ganzhiwuxin",
     version = "0.1",
     packages = find_packages(),
-    scripts = ['ganzhiwuxin.py'],
+    #packages = ['ganzhiwuxin'],
+    #py_modules= ['ganzhiwuxin'],
+    zip_safe = False,
+    #scripts = ['ganzhiwuxin.py'],
     # Project uses reStructuredText, so ensure that the docutils get
     # installed or upgraded on the target machine
-    install_requires = ['docutils>=0.3'],
+    #install_requires = ['docutils>=0.3'],
     #package_data = {
     #    # If any package contains *.txt or *.rst files, include them:
     #    '': ['*.txt', '*.rst'],
